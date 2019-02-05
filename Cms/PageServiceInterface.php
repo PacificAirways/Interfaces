6d720990d464e1d5bf74b16bb67b725b3bc7a7ce
Personal <me@mattbutterworth.co.uk>
2019-02-05 21:20:21 +0000

    Add throws exception to PageServiceInterface

--- Cms/PageServiceInterface.php
+++ Cms/PageServiceInterface.php
@@ -1,11 +1,14 @@
 <?php namespace Pacificairways\Interfaces\Cms;
 
+use Pacificairways\Interfaces\Cms\Exception\ContentNotFoundException;
+
 interface PageServiceInterface
 {
     /**
      * @param string $slug
      *
      * @return PageInterface
+     * @throws ContentNotFoundException
      */
     public function getBySlug($slug);
 
